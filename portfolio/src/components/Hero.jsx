0f6eb2b550f8454dc95e99a45b7b458cbccb4372
sharxav <sharxav@gmail.com>
2023-08-29 14:38:50 -0400

    msg

--- portfolio/src/components/Hero.jsx
+++ portfolio/src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
         <section className={`relative w-full h-screen mx-auto`}>
            
             <div
-                className={`absolute inset-0 top-[150px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-col items-center gap-2`}
+                className={`absolute inset-0 top-[150px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-col text-center items-center gap-2`}
             >
              
                 
@@ -20,7 +20,7 @@ const Hero = () => {
          
            
                     <p className='text-white-100 text-center'>
-                        Software Developer | Data Analyst | Recent Computer Science Graduate
+                        Software Developer | Data Analyst 
                     </p>
                     <div className="mt-2 flex justify-center items-center gap-2">
                         <img src={github} alt="my github" width="20" height="20" onClick={() => window.open("https://github.com/sharxav", "_blank")} />
